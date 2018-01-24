b2693913075b01594e71e094f080cba4de8a3c66
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-01-24 03:10:31 +0100

    added "rain" volume control by mousePosition

--- app/components/Rain/index.js
+++ app/components/Rain/index.js
@@ -6,18 +6,52 @@ import rainHeavy from "./rain-heavy.mp3";
 import Audio from "../Audio";
 
 const Rain = {
+  // handle interaction
+  mouseHandler(e) {
+    this.updateVolume(e.clientX, e.clientY);
+  },
+
+  touchHandler(e) {
+    // e.preventDefault();
+    // const touch = e.touches[0];
+    this.updateVolume(e.touches[0].clientX, e.touches[0].clientY);
+  },
+
+  updateVolume(x, y) {
+    x = x / this.width;
+    y = y / this.height;
+    // const yInverse = (1 - y);
+
+    Audio.setSoundVolume("rainHeavy", y);
+    // this.dropDelay = yInverse * yInverse * yInverse * 100 + 2;
+    // this.wind = (x - 0.5) * 50;
+  },
+
+  resize() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.canvas.width = this.width * this.dpr;
+    this.canvas.height = this.height * this.dpr;
+  },
+
   init() {
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.dpr = window.devicePixelRatio || 1;
+    this.resize();
+
     Audio.loadFile(rainSoft, "rainSoft", () => {
       Audio.createAudioInfinite("rainSoft", true);
     });
     Audio.loadFile(rainHeavy, "rainHeavy", () => {
       Audio.createAudioInfinite("rainHeavy", false);
     });
+
+    document.addEventListener("mousemove", this.mouseHandler.bind(this));
+    document.addEventListener("touchstart", this.touchHandler.bind(this));
+    document.addEventListener("touchmove", this.touchHandler.bind(this));
+    window.addEventListener("resize", this.resize.bind(this));
   },
 };
 
 export default Rain;
-
-// setTimeout(() => {
-//   Audio.setSoundVolume("rainHeavy", 1);
-// }, 5000);
